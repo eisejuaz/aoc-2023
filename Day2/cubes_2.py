9af0bd5be9472fb71cdb27c27bcc7b48b467879e
Franco Gargiulo <animalitof@gmail.com>
2023-12-02 14:31:17 -0300

    day 2 complete

---
+++ Day2/cubes_2.py
@@ -0,0 +1,4 @@
+# this one was also solved in excel, as it was really easy to use conditional formatting after some text to columns splitting
+# I had to somehow redo day1 due to a file corruption hahahahah
+# it was easier than part one, but I took extra care in data cleaning
+# I got the max value of each color in each game and multiplied it
